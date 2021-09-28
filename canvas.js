2ba44524901731fcb4f53b18ef755cdf9801842a
joao Castro <joao_lukas97@hotmail.com>
2021-09-28 19:36:29 -0300

    fill function

--- canvas.js
+++ canvas.js
@@ -52,6 +52,7 @@ function Circle(x, y, dx, dy, radius) {
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.strokeStyle = 'blue';
         c.stroke();
+        c.fill();
     }
 
     this.update = function () {
